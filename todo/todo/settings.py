646dcaf19dec8991a8b0ca056733d371e7739f25
Abhilash Hari <abhilashhari1313@gmail.com>
2023-09-03 15:48:26 +0530

    Updated

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,7 +74,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -83,7 +84,11 @@ DATABASES = {
         'HOST':'localhost'
     }
 }
+'''
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -120,6 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from tasks import urls          
+from tasks import urls 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns         
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('tasks.urls'))
 ]
+urlpatterns += staticfiles_urlpatterns()
