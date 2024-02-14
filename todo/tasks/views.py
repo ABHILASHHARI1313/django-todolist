fdf8b3e0ea617c618c3430a7d26711b33594cd45
Abhilash Hari <abhilashhari1313@gmail.com>
2024-02-14 22:12:20 +0530

    Day 01

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,41 +1,9 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import HttpResponse
-from .models import *
-from .forms import *
+from .models import Task
 
 # Create your views here.
-
 def index(request):
-    tasks =  Task.objects.all()
+    tasks = Task.objects.all()
+    return render(request,'tasks/list.html')
 
-    form = TaskForm()
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-
-    context = {'tasks':tasks,'form':form}
-    return render(request,'tasks/list.html',context)
-
-def updateTask(request,pk):
-    task =  Task.objects.get(id=pk)
-    form =TaskForm(instance=task)
-    
-    context = {'form':form}
-    if request.method == 'POST':
-        form =TaskForm(request.POST,instance=task)
-        if form.is_valid():
-            form.save()
-        return redirect('/')
-
-    return render(request,'tasks/update_task.html',context)
-
-def deleteTask(request,pk):
-    item =  Task.objects.get(id=pk)
-
-    if request.method == 'POST':
-        item.delete()
-        return redirect('/')
-    context = {'item':item}
-    return render(request,'tasks/delete.html',context)
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for todo project.
+"""todo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,11 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from tasks import urls 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns         
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('tasks.urls'))
+    path("",include("tasks.urls"))
 ]
-urlpatterns += staticfiles_urlpatterns()
